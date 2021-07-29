fe7783e9586771b062a3a4a17891a00e337e1b40
feden2906 <feden2906@gmail.com>
2021-07-29 21:42:02 +0300

    test change theme in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,18 @@ import { UserInfo } from "../UserInfo";
 import { SearchPanel } from "../SearchPanel";
 import { Link } from "react-router-dom";
 import { GenresSelect } from "../GenresSelect";
+import { ToggleThem } from "../ToggleThem";
+import { useSelector } from "react-redux";
 
 export const Header = () => {
+  const { theme } = useSelector(({theme}) => theme);
+  console.log(theme)
   return (
-      <div className={styles.header}>
+      <div className={`${styles.header} ${theme ? styles.white : styles.dark}`}>
         <Link to="/" className={styles.logo}><b><i>MoviesDB</i></b></Link>
         <SearchPanel/>
         <GenresSelect/>
+        <ToggleThem/>
         <UserInfo/>
       </div>
   );
