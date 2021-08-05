7bd0aec5f26940ba44aa60e952b17699b64ed88b
Fedenko Maksym <feden2906@gmail.com>
2021-08-06 01:33:39 +0300

    add icons to toggle theme

--- src/components/ToggleThem/ToggleThem.js
+++ src/components/ToggleThem/ToggleThem.js
@@ -2,6 +2,7 @@ import Switch from '@material-ui/core/Switch';
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
+import styles from "./ToggleThem.module.css";
 import { setTheme } from "../../redux";
 
 export const ToggleThem = () => {
@@ -21,8 +22,13 @@ export const ToggleThem = () => {
   }
 
   return (
-      <div>
+      <div className={styles.wrapper}>
+        <img src={state ? './sun1.png' : './sun2.png'} alt="sun icon"/>
         <Switch checked={state} onChange={changeTheme} name="toggle" color="secondary"/>
+        <img src={state ? './moon1.png' : './moon2.png'} alt="sun icon"/>
+        {/*<img src={state ? './cloud.png' : './sun.png'} alt="sun icon"/>*/}
+        {/*<Switch checked={state} onChange={changeTheme} name="toggle" color="secondary"/>*/}
+        {/*<img src={state ? './moon.png' : './cloud.png'} alt="sun icon"/>*/}
       </div>
   );
 }
