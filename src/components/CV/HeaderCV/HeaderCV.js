cde494592edba2dee810cedd23fd98bde7813bd3
feden2906 <feden2906@gmail.com>
2021-08-08 00:10:53 +0300

    fix photo)

--- src/components/CV/HeaderCV/HeaderCV.js
+++ src/components/CV/HeaderCV/HeaderCV.js
@@ -4,7 +4,7 @@ export const HeaderCV = () => {
 
   return (
       <div className={styles.header}>
-        <img className={styles.img} src="./img.png" alt="myPhoto"/>
+        <img className={styles.img} src="./myPhoto.png" alt="myPhoto"/>
         <div className={styles.info}>
           <h1>MAKSYM FEDENKO</h1>
           <p>DEVELOPER</p>
